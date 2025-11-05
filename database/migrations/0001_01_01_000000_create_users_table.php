7137bfe753e993b64c3a1f8d7e4a7b43a4931f97
Samuel1000-90 <samueldg0210@gmail.com>
2025-11-05 14:09:55 -0500

    s

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
+            $table->string('role')->default('usuario'); // puede ser 'admin' o 'usuario'
             $table->timestamp('created_at')->nullable();
         });
 
